85557ff4b7c52f0e43fcb3541332be6b85c856b2
Leon Ljunghorn <leonljunghorn@gmail.com>
2022-10-01 13:21:22 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '1.0.0');
+	define('_S_VERSION', '41.0.0');
 }
 
 /**
@@ -328,9 +328,9 @@ function ob_plug_register_required_plugins()
 		'default_path' => '', // Default absolute path to bundled plugins.
 		'menu' => 'tgmpa-install-plugins', // Menu slug.
 		'parent_slug' => 'themes.php', // Parent menu slug.
-		'capability' => 'edit_theme_options', // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'capability' => 'plugins', // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
 		'has_notices' => true, // Show admin notices or not.
-		'dismissable' => true, // If false, a user cannot dismiss the nag message.
+		'dismissable' => false, // If false, a user cannot dismiss the nag message.
 		'dismiss_msg' => 'ooooooooooooooo', // If 'dismissable' is false, this message will be output at top of nag.
 		'is_automatic' => true, // Automatically activate plugins after installation or not.
 		'message' => 'heeeeeeeeeeeej', // Message to output right before the plugins table.
