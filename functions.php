07c676b11ca3fc7bb42ee93c0c6885ad2be386a3
Leon Ljunghorn <leonljunghorn@gmail.com>
2022-10-18 20:02:46 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -300,7 +300,8 @@ function ob_plug_register_required_plugins()
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 			'external_url' => '', // If set, overrides default API URL and points to an external URL.
 			'is_callable' => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-		),
+		),	
+		
 			array(
 			'name' => '1-lightbox-webp', // The plugin name.
 			'slug' => '1-lightbox', // The plugin slug (typically the folder name).
@@ -308,9 +309,10 @@ function ob_plug_register_required_plugins()
 			'version' => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation' => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url' => 'https://github.com/symbolen/1-lightbox/archive/refs/heads/main.zip', // If set, overrides default API URL and points to an external URL.
+			'source' => 'https://github.com/symbolen/1-lightbox/archive/refs/heads/main.zip', // If set, overrides default API URL and points to an external URL.
 			'is_callable' => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
-		),		
+		),
+		
 			array(
 			'name' => 'fastdup', // The plugin name.
 			'slug' => 'fastdup', // The plugin slug (typically the folder name).
